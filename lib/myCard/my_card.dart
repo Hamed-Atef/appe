d2344c928bdefb1698a96389af7202628e21cfbf
Hamed-Atef <sayedhamedatef@gmail.com>
2025-02-06 14:43:08 +0430

    my_card.dart is updated

--- lib/myCard/my_card.dart
+++ lib/myCard/my_card.dart
@@ -4,9 +4,11 @@ import 'package:flutter/material.dart';
 class MiCardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Mi Card")),
-      body: Center(child: Text("Hello World")),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Mi Card")),
+        body: Container(child: Text("Hello World")),
+      ),
     );
   }
 }
